a367a9aa04f91359718fbc9107d1833583524820
bebr001 <bebr0001@student.monash.edu>
2023-05-07 22:07:08 +1000

    add viewmodel in home fragment and change the message for creating account

--- app/src/main/java/com/example/goal/fragment/HomeFragment.java
+++ app/src/main/java/com/example/goal/fragment/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.example.goal.R;
 import com.example.goal.databinding.HomeFragmentBinding;
 import com.example.goal.entity.Customer;
+import com.example.goal.viewmodel.CustomerViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +28,7 @@ public class HomeFragment extends Fragment {
     private FirebaseAuth authProfile;
     private TextView textViewWelcome;
     private String name,email,address;
+    private CustomerViewModel customerViewModel;
     public HomeFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,15 +55,19 @@ public class HomeFragment extends Fragment {
     }
     private void showUserProfile(FirebaseUser firebaseUser){
         String useId = firebaseUser.getUid();
-        System.out.println();
         DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("User");
         referenceProfile.child(useId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //customerViewModel = ViewModelProviders.of(this).get(CustomerViewModel.class);
                 Customer customer = snapshot.getValue(Customer.class);
                 if(customer != null){
                     name = customer.getName();
                     email = customer.getEmail();
+                    address = customer.getAddress();
+                    // save in local database
+                    //customerViewModel.insert(temp);
+
                     textViewWelcome.setText("Welcome " + name + "!");
                 }
             }
