76a6dc67ba5aa5f785936a2fb2bbb56f8c9706ca
Samia Senzuti <samia.senzuti@monash.edu>
2023-05-12 15:04:23 +1000

    Goals shown according to customer id

--- app/src/main/java/com/example/goal/fragment/AllGoals.java
+++ app/src/main/java/com/example/goal/fragment/AllGoals.java
@@ -1,5 +1,7 @@
 package com.example.goal.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -17,6 +19,7 @@ import com.example.goal.entity.Goal;
 import com.example.goal.recycler.RecyclerViewAdapter;
 import com.example.goal.viewmodel.GoalViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -25,6 +28,7 @@ public class AllGoals extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerViewAdapter recyclerViewAdapter;
     private GoalViewModel goalViewModel;
+    private int loggedInUserId; // Assuming you have the logged-in user's ID
 
     public AllGoals() {
         // Required empty public constructor
@@ -40,7 +44,6 @@ public class AllGoals extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_all_goals, container, false);
 
         recyclerView = rootView.findViewById(R.id.recyclerView);
-        //goalViewModel = new ViewModelProvider(this).get(GoalViewModel.class);
         goalViewModel = new ViewModelProvider(this).get(GoalViewModel.class);
 
         recyclerViewAdapter = new RecyclerViewAdapter(goalViewModel);
@@ -49,16 +52,32 @@ public class AllGoals extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(recyclerViewAdapter);
 
+        // Retrieve the logged-in user's ID from shared preferences
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        loggedInUserId = sharedPreferences.getInt("customerId", 0);
 
         // Observe the goals LiveData in the GoalViewModel
         goalViewModel.getGoals().observe(getViewLifecycleOwner(), new Observer<List<Goal>>() {
             @Override
             public void onChanged(List<Goal> goals) {
-                // Update the RecyclerView adapter with the new list of goals
-                recyclerViewAdapter.setGoals(goals);
+                // Filter the goals based on the logged-in user's ID
+                List<Goal> filteredGoals = filterGoalsByUserId(goals, loggedInUserId);
+
+                // Update the RecyclerView adapter with the filtered list of goals
+                recyclerViewAdapter.setGoals(filteredGoals);
             }
         });
 
         return rootView;
+    }
+
+    private List<Goal> filterGoalsByUserId(List<Goal> goals, int userId) {
+        List<Goal> filteredGoals = new ArrayList<>();
+        for (Goal goal : goals) {
+            if (goal.getCustomerId() == userId) {
+                filteredGoals.add(goal);
+            }
+        }
+        return filteredGoals;
     }
 }
