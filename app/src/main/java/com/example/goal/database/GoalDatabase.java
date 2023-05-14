3b48fdaf6cbacf3a8528c1e6a6f616deb5d006ec
Samia Senzuti <samia.senzuti@monash.edu>
2023-05-14 12:22:30 +1000

    db changes

--- app/src/main/java/com/example/goal/database/GoalDatabase.java
+++ app/src/main/java/com/example/goal/database/GoalDatabase.java
@@ -9,13 +9,22 @@ import androidx.room.RoomDatabase;
 import com.example.goal.dao.GoalDAO;
 import com.example.goal.entity.Customer;
 import com.example.goal.entity.Goal;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Customer.class, Goal.class}, version = 4)
 public abstract class GoalDatabase extends RoomDatabase {
     private static GoalDatabase instance;
 
     public abstract GoalDAO goalDao();
 
-    public static synchronized GoalDatabase getInstance(Context context) {
+    private static final int NUMBER_OF_THREADS = 10;
+
+    public static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static synchronized GoalDatabase getInstance(final Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                             GoalDatabase.class, "goal_database")
